5c7d34878badeb7e27faeebcb4469655091f216f
zaheer <zaheerk0302@gmail.com>
2021-12-21 16:42:14 +0530

    initial

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express=require("express");
 const app=express();
 const path=require('path');
 const cors=require('cors');
-const PORT=process.env.PORT || 3000;
+const PORT=process.env.PORT || 5500;
 
 
 
